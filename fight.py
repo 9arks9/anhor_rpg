83b714079e2c896a2942b21b294bf4c39d66a8ca
9arks9 <9arks9@gmail.com>
2025-05-23 18:10:14 +0200

    Update fight.py
    Adding [GameLogger] compatibility

--- fight.py
+++ fight.py
@@ -1,4 +1,6 @@
 import random
+from utils import GameLogger
+logger = GameLogger()
 
 class FightModel:
     def __init__(self, hero, enemy, logger=None):
@@ -26,14 +28,13 @@ class FightModel:
         return max((attack * crit) - enemy_defensive, 0)
 
     def run(self):
-        if self.logger:
-            self.logger.add_fight_log('==================== FIGHT STARTED ====================')
+        logger.log('==================== FIGHT STARTED ====================','fight')
         # Fight loop while both characters are alive
         while self.enemy_hp > 0 and self.hero_hp > 0:
             # Hero's turn
             h_dmg = self.damage_calculator(self.hero_attack, self.enemy_defence)
             if h_dmg <= 0:
-                print(f'{self.hero.name} missed the attack.')
+                logger.log(f'{self.hero.name} missed the attack.','fight')
             self.enemy_hp -= h_dmg
             self.h_total_dmg += h_dmg
             if h_dmg > self.hero_attack - self.enemy_defence and h_dmg > 0:
@@ -43,14 +44,14 @@ class FightModel:
                     h_dmg = str(h_dmg) + ' (Critical)'
             if self.enemy_hp < 0:
                 self.enemy_hp = 0
-            print(f'{self.hero.name} --dealt {h_dmg} damage to-- {self.enemy.name}.')
-            print(f'{self.enemy.name} HP: {self.enemy_hp}/{self.enemy.hp}\n___________')
+            logger.log(f'{self.hero.name} --dealt {h_dmg} damage to-- {self.enemy.name}.','fight')
+            logger.log(f'{self.enemy.name} HP: {self.enemy_hp}/{self.enemy.hp}\n___________','fight')
             if self.enemy_hp <= 0:
                 break
             # Enemy's turn
             e_dmg = self.damage_calculator(self.enemy_attack, self.hero_defence)
             if e_dmg <= 0:
-                print(f'{self.enemy.name} missed the attack.')
+                logger.log(f'{self.enemy.name} missed the attack.','fight')
             self.hero_hp -= e_dmg
             self.e_total_dmg += e_dmg
             if e_dmg > self.enemy_attack - self.hero_defence and e_dmg > 0:
@@ -60,16 +61,16 @@ class FightModel:
                     e_dmg = str(e_dmg) + ' (Critical)'
             if self.hero_hp < 0:
                 self.hero_hp = 0
-            print(f'{self.enemy.name} --dealt {e_dmg} damage to-- {self.hero.name}.')
-            print(f'{self.hero.name} HP: {self.hero_hp}/{self.hero.hp}\n___________')
+            logger.log(f'{self.enemy.name} --dealt {e_dmg} damage to-- {self.hero.name}.','fight')
+            logger.log(f'{self.hero.name} HP: {self.hero_hp}/{self.hero.hp}\n___________','fight')
         # Display Fight result
         if self.hero_hp <= 0:
-            print(f'{self.hero.name} is dead.')
+            logger.log(f'{self.hero.name} is dead.','fight)
         elif self.enemy_hp <= 0:
-            print(f'{self.enemy.name} is dead.')
+            logger.log(f'{self.enemy.name} is dead.','fight')
             self.hero.add_exp(self.enemy.level * 5)
             self.enemy.add_death(1)
-            print(f'{self.hero.name} gained {self.enemy.level * 5} experience points.')
+            logger log(f'{self.hero.name} gained {self.enemy.level * 5} experience points.','fight')
         else:
-            print('Draw')
-        return f'\n**************************************\nHero: {self.hero.name} dealt {self.h_total_dmg} damage.\nEnemy: {self.enemy.name} dealt {self.e_total_dmg} damage.\n**************************************\n----------------------------------------------------------'
+            logger.log('Draw','fight')
+        logger.log(f'\n**************************************\nHero: {self.hero.name} dealt {self.h_total_dmg} damage.\nEnemy: {self.enemy.name} dealt {self.e_total_dmg} damage.\n**************************************\n----------------------------------------------------------,'fight')
