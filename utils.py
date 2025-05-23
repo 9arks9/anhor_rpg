d8f2c5a8ac6a019cdc468e8a96aa28d51153c077
9arks9 <9arks9@gmail.com>
2025-05-23 18:32:13 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,24 +1,24 @@
 class GameLogger:
     def __init__(self):
         self.fight_log = []
-        self.event_log = []
-        self.system_log = []
+        self.level_log = []
+        self.enemy_log = []
 
     def log(self, msg, log_type="fight"):
         if log_type == "fight":
             self.fight_log.append(msg)
-        elif log_type == "event":
-            self.event_log.append(msg)
-        elif log_type == "system":
-            self.system_log.append(msg)
+        elif log_type == "l":
+            self.level_log.append(msg)
+        elif log_type == "e":
+            self.enemy_log.append(msg)
 
     def get_log(self, log_type="fight"):
         if log_type == "fight":
             return "\n".join(self.fight_log)
-        elif log_type == "event":
-            return "\n".join(self.event_log)
-        elif log_type == "system":
-            return "\n".join(self.system_log)
+        elif log_type == "l":
+            return "\n".join(self.level_log)
+        elif log_type == "e":
+            return "\n".join(self.enemy_log)
         return ""
 
 # Global logger
